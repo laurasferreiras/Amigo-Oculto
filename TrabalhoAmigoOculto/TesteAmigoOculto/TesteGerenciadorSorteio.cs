bea3a71791ad2eff58346e7df8694705ca462acf
laurasferreiras <laurasferreiras6@gmail.com>
2025-09-15 22:10:44 -0300

    testes 2 criando

---
+++ TrabalhoAmigoOculto/TesteAmigoOculto/TesteGerenciadorSorteio.cs
@@ -0,0 +1,22 @@
+﻿using TrabalhoAmigoOculto.classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Intrinsics.Arm;
+
+namespace TesteAmigoOculto
+{
+    [TestClass]
+    public class TesteGerenciadorSorteio
+    {
+        [TestMethod]
+        public void TestSortearSemRepeticao()
+        {
+            var pessoas = new List<string>["Laura", "Bruna", "Julia", "Carlos", "Gabriel"];
+            GerenciadorSorteio gerenciadorSorteio = new GerenciadorSorteio();
+
+        }
+    }
+}

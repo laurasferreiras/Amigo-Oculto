50f4f06693161cefa62e977ad24d644cd63fe81f
laurasferreiras <laurasferreiras6@gmail.com>
2025-09-15 21:42:48 -0300

    primeiro teste pronto

---
+++ TrabalhoAmigoOculto/TesteAmigoOculto/TesteGerenciadorPessoas.cs
@@ -0,0 +1,25 @@
+﻿using TrabalhoAmigoOculto.classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteAmigoOculto
+{
+    [TestClass]
+    public class TesteGerenciadorPessoas
+    {
+        [TestMethod]
+        public void TestGerenciadorPessoa()
+        {
+            GerenciadorPessoas gerenciadorPessoas = new GerenciadorPessoas();
+            string nome = "Laura";
+
+            gerenciadorPessoas.AdicionarPessoa(nome);
+
+            bool existe = gerenciadorPessoas.ObterPessoas().Contains(nome);
+            Assert.IsTrue(existe, "Foi adicionado");
+        }
+    }
+}
